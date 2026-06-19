1f74001023c504c81f02ad1d93a8530144e3466f
Abhishek Vibhute <abhishekvibhute2200@gmail.com>
2026-06-19 15:18:42 +0530

    fix: update mailer configuration for improved connection handling

--- Backend/controllers/user/auth/mailer.js
+++ Backend/controllers/user/auth/mailer.js
@@ -3,11 +3,26 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+      connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000
 })
+
+
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("MAIL VERIFY ERROR:", error);
+    } else {
+        console.log("MAIL SERVER READY");
+    }
+});
 
 module.exports = transporter;
